30633abf692cc97584409482e31bc9f5d1c38cda
Andre Reynaldi Lusikooy <andre.reynaldi@ai.astra.co.id>
2024-03-20 13:12:10 +0700

    add image multipart

--- src/index.js
+++ src/index.js
@@ -26,4 +26,73 @@ const endpoints = (token, body) => {
         });
 };
 
+export const uploadImage = async (token, filePath, microservices) => {
+    const fileName = filePath.split('/').slice(-1).join('');
+    const microservicesToCamelize = microservices.split(/\s+/)
+    .map(function(word, index) {
+      return index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join('');
+
+    // Upload Request
+    const paramUploadRequest = {
+        input: {
+            fileName
+        }
+    }
+
+    const uploadRequest = () => ({
+        mutation: {
+            [`uploadRequestFrom${microservicesToCamelize}`]: {
+                __args: paramUploadRequest,
+                resourceId: true,
+                token: true
+            }
+        }
+    })
+
+    const responseUploadRequest = await endpoints(token, uploadRequest());
+    const dataUploadRequest = responseUploadRequest.body.data[`uploadRequestFrom${microservicesToCamelize}`];
+    expect(dataUploadRequest.hasOwnProperty('resourceId')).toBe(true);
+
+    const url = dataUploadRequest.token;
+    const resourceId = dataUploadRequest.resourceId;
+
+    // PUT Image
+    const fileData = fs.readFileSync(filePath);
+
+    const responseImage = await axios({
+        method: "put",
+        url,
+        data: Buffer.from(fileData),
+        headers: { "Content-Type": "multipart/form-data", "x-ms-blob-type": "BlockBlob" }
+    });
+
+    expect(responseImage.status).toBe(201);
+
+    // Upload Complete
+    const paramuploadComplete = {
+        input: {
+            resourceId,
+            token: url
+        }
+    }
+
+    const uploadComplete = () => ({
+        mutation: {
+            [`uploadCompleteFrom${microservicesToCamelize}`]: {
+                __args: paramuploadComplete,
+                operationId: true
+            }
+        }
+    })
+
+    const responseuploadComplete = await endpoints(token, uploadComplete());
+    const dataUploadComplete = responseuploadComplete.body.data[`uploadCompleteFrom${microservicesToCamelize}`];
+
+    expect(dataUploadComplete.hasOwnProperty('operationId')).toBe(true);
+
+    return {operationId: dataUploadComplete.operationId, url};
+}
+
 module.exports = { writeFileEncrypted, endpoints };
